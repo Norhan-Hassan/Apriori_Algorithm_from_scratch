328ada90a2c25e1433d3904f7b0fd565760e68da
Norhan Hassan <nwrhanhsnly@gmail.com>
2024-04-20 14:51:00 +0200

    some changes

--- Apriori_Algorithm.py
+++ Apriori_Algorithm.py
@@ -1,3 +1,6 @@
+#Norhan Hassan
+# 20201202
+
 import tkinter as tk
 from collections import Counter
 from itertools import combinations
@@ -10,7 +13,7 @@ class AprioriWithGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Apriori Algorithm GUI")
-        self.root.configure(bg="#F5F5DC")
+        self.root.configure(bg="#F0F0DC")
 
         # GUI elements
         tk.Label(root, text="Min Support Count:").pack()
@@ -25,7 +28,7 @@ class AprioriWithGUI:
         self.percentage_entry = tk.Entry(root)
         self.percentage_entry.pack()
 
-        self.preprocess_button = tk.Button(root, text="Choose File", command=self.preprocess_and_find_association_rules)
+        self.preprocess_button = tk.Button(root, text="Choose File", command=self.calling_function)
         self.preprocess_button.pack()
 
         # Text widget to display association rules
@@ -36,7 +39,7 @@ class AprioriWithGUI:
         self.association_rules_text = tk.Text(root, height=10, width=50)
         self.association_rules_text.pack()
 
-    def preprocess_and_find_association_rules(self):
+    def calling_function(self):
         file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
         if not file_path:
             return
@@ -53,18 +56,17 @@ class AprioriWithGUI:
         data = self.preprocess_data(df)
 
         # Find frequent item sets
-        frequent_item_sets = self.find_frequent_item_sets(data, min_support_count)
+        frequent_item_sets, last_frequent_item_sets = self.find_frequent_item_sets(data, min_support_count)
 
         if frequent_item_sets:
-            # Display frequent item sets
             self.display_frequent_itemset(frequent_item_sets)
-            # Generate association rules
-            association_rules = self.generate_association_rules(frequent_item_sets, min_confidence)
-            print("Association Rules generated :", association_rules)  
+
+            association_rules = self.generate_association_rules(last_frequent_item_sets, frequent_item_sets,min_confidence)
+
+            print("Association Rules generated :", association_rules)
 
         # Display association rules
         self.display_association_rules(association_rules)
-
 
     def preprocess_data(self, df):
         # Check for missing values
@@ -85,14 +87,14 @@ class AprioriWithGUI:
         all_items = [item for sublist in data['Items'] for item in sublist]
         unique_items = set(all_items)
 
-        # Initialize k to 1 for the first iteration
         k = 1
         Lk_1 = []
         frequent_item_sets = {}
+        last_frequent_item_sets = {}
+
         while True:
             print("Processing iteration", k)
 
-            # Generate candidate itemsets Ck
             if k == 1:
                 Ck_items = {frozenset([item]) for item in unique_items}
             else:
@@ -106,6 +108,8 @@ class AprioriWithGUI:
             if not Lk:
                 print("No frequent itemsets found for iteration", k)
                 break
+            else:
+                last_frequent_item_sets = Lk
 
             # Prepare for the next iteration
             k += 1
@@ -113,14 +117,14 @@ class AprioriWithGUI:
             frequent_item_sets.update(Lk)
 
         print("Apriori algorithm completed")
-        return frequent_item_sets
+        return frequent_item_sets, last_frequent_item_sets
 
     def generate_candidate_itemsets(self, Lk_1, k):
         Ck_items = set()
-        for itemset1 in Lk_1:
-            for itemset2 in Lk_1:
-                if len(itemset1.union(itemset2)) == k:
-                    Ck_items.add(itemset1.union(itemset2))
+        for itemset_1 in Lk_1:
+            for itemset_2 in Lk_1:
+                if len(itemset_1.union(itemset_2)) == k:
+                    Ck_items.add(itemset_1.union(itemset_2))
         return Ck_items
 
     def generate_frequent_itemsets(self, data, Ck_items, min_support_count):
@@ -134,51 +138,47 @@ class AprioriWithGUI:
             support_count >= min_support_count}
         return Lk
 
-    def generate_association_rules(self, frequent_item_sets, min_confidence):
+    def generate_association_rules(self, last_frequent_item_sets, frequent_item_sets, min_confidence):
         association_rules = []
-        for itemset in frequent_item_sets.keys():
+        for itemset in last_frequent_item_sets.keys():
             if len(itemset) > 1:
                 self.generate_rules_from_itemset(itemset, frequent_item_sets, association_rules, min_confidence)
-        print("Association Rules:", association_rules)  
+        print("Association Rules (Last Iteration):", association_rules)
         return association_rules
 
     def generate_rules_from_itemset(self, itemset, frequent_item_sets, association_rules, min_confidence):
-        for i in range(len(itemset) - 1, 0, -1):  # Start from the largest combination
-            for LHS in combinations(itemset, i):
-                LHS_set = frozenset(LHS)
-                confidence = frequent_item_sets[itemset] / frequent_item_sets[LHS_set]
+        for i in range(len(itemset) - 1, 0, -1):
+            for Left_Hand_Side in combinations(itemset, i):
+                Left_Hand_Side_set = frozenset(Left_Hand_Side)
+                confidence = frequent_item_sets[itemset] / frequent_item_sets[Left_Hand_Side_set]
                 if confidence >= min_confidence:
-                    RHS = itemset.difference(LHS_set)
-                    association_rules.append((LHS, RHS, confidence))
+                    Right_Hand_Side = itemset.difference(Left_Hand_Side_set)
+                    association_rules.append((Left_Hand_Side, Right_Hand_Side, confidence))
 
     def display_frequent_itemset(self, frequent_item_sets):
-        self.frequent_item_sets_text.delete(1.0, tk.END)  
+        self.frequent_item_sets_text.delete(1.0, tk.END)
         self.frequent_item_sets_text.insert(tk.END, "Frequent Item Sets:\n")
 
         # Display each item set
         for item_set in frequent_item_sets:
-            items_str = ", ".join(item_set)
+            items_str = " , ".join(item_set)
             support_count = frequent_item_sets[item_set]
             self.frequent_item_sets_text.insert(tk.END, f"Item Set: {{{items_str}}}, Support Count: {support_count}\n")
 
         self.frequent_item_sets_text.pack(fill=tk.BOTH, expand=True)
 
-
-
     def display_association_rules(self, association_rules):
-        self.association_rules_text.delete(1.0, tk.END)  
+        self.association_rules_text.delete(1.0, tk.END)
         self.association_rules_text.insert(tk.END, "Strong Association Rules:\n")
         for rule in association_rules:
-            LHS = "{" + ', '.join(rule[0]) + "}"
-            RHS = "{" + ', '.join(rule[1]) + "}"
+            LHS = "{" + ' , '.join(rule[0]) + "}"
+            RHS = "{" + ' , '.join(rule[1]) + "}"
             self.association_rules_text.insert(tk.END, f"Rule: {LHS} => {RHS}, Confidence: {rule[2]}\n")
             self.association_rules_text.pack(fill=tk.BOTH, expand=True)
-
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = AprioriWithGUI(root)
-    # Set window size and allow it to expand
-    root.resizable(True, True)  # Allow both horizontal and vertical resizing
+    root.resizable(True, True)
     root.mainloop()
